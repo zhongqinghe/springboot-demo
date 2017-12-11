c6e065bda06a76510450bffd0830642d50ea09a2
zhongqinghe <zhongqinghe@qq.com>
2017-12-11 18:28:14 +0800

    Initial commit

---
+++ src/main/java/com/zhong/demo/HelloController.java
@@ -0,0 +1,12 @@
+package com.zhong.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
+}
