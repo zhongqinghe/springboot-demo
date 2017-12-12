dd470a1ce83d1ef6e11315a9b985da552412d58d
zhongqinghe <zhongqinghe@qq.com>
2017-12-12 11:11:36 +0800

    增加spring配置文件选择

--- src/main/java/com/zhong/demo/HelloController.java
+++ src/main/java/com/zhong/demo/HelloController.java
@@ -1,12 +1,24 @@
 package com.zhong.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    @Value("${com.zhong.helloWorld}")
+    private String helloWorld;
+
+    public String getHelloWorld() {
+        return helloWorld;
+    }
+
+    public void setHelloWorld(String helloWorld) {
+        this.helloWorld = helloWorld;
+    }
+
     @RequestMapping("/hello")
     public String hello(){
-        return "hello world22!";
+        return helloWorld;
     }
 }
